0feaa8d9e598e7c04750b8cb49e8d0da436f5238
Ivan Simon <isimon@finametrix.com>
2020-10-16 10:31:48 +0200

    nuevas librerias y refactorizado app

--- disid-web/src/app/app.module.ts
+++ disid-web/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { DepartamentosComponent } from './departamentos/departamentos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent,
+    DepartamentosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    AppRoutingModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
