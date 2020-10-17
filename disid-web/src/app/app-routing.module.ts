2f8c89083cab3c7f3b6601ec1d19c17ca9f9177e
Ivan Simon <isimon@finametrix.com>
2020-10-17 17:51:36 +0200

    add moles and routes

--- disid-web/src/app/app-routing.module.ts
+++ disid-web/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employee/employee.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewOrEditEmployeeComponent } from './new-or-edit-employee/new-or-edit-employee.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmployeesComponent },
+  { path: 'employee/edit', component: NewOrEditEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- disid-web/src/app/app.module.ts
+++ disid-web/src/app/app.module.ts
@@ -1,23 +1,35 @@
+import { Employee } from './employee/employee.model';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmpleadosComponent } from './empleados/empleados.component';
-import { DepartamentosComponent } from './departamentos/departamentos.component';
+import { EmployeesComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+
 import { InterceptService } from './intercept.service';
+import { NewOrEditEmployeeComponent } from './new-or-edit-employee/new-or-edit-employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadosComponent,
-    DepartamentosComponent
+    EmployeesComponent,
+    NewOrEditEmployeeComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
@@ -27,8 +39,13 @@ import { InterceptService } from './intercept.service';
     MatSelectModule,
     HttpClientModule,
     MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatMenuModule,
     AppRoutingModule,
-    MatFormFieldModule
+    MatTabsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   providers: [
     InterceptService,
@@ -38,6 +55,10 @@ import { InterceptService } from './intercept.service';
       multi: true
     }
   ],
+  exports: [
+    FormsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
